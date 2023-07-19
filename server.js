7008e9be6bd3f503827e87aa7077a05ec26ea7b0
Dylan Kazunaga <dylankazunaga@Dylans-MacBook-Air.local>
2023-07-18 20:19:51 -0700

    added a server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 5503; // Change this to the desired port number
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
